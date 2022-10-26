f629cc699358b72ffdec83fef0d53a1659de2986
poseidon <jsy10303@gmail.com>
2022-10-26 16:08:28 +0900

    초기 작업 01

---
+++ android/app/src/main/kotlin/com/jsy/netflix_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jsy.netflix_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
